fe67f38554d6198436a555ec4181f690cde3ff81
ErrHu <erichu07@gmail.com>
2015-03-23 23:45:59 -0400

    reverting bad migration file changes

--- course/migrations/0001_initial.py
+++ course/migrations/0001_initial.py
@@ -3,20 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-from django.core.management import call_command
-
-
-fixture_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../fixtures'))
-fixture_filename = 'initial_data.json'
-fixture_file = os.path.join(fixture_dir, fixture_filename)
-
-
-def load_fixture(apps, schema_editor):
-    call_command('loaddata', fixture_file) 
-    
 
 class Migration(migrations.Migration):
-    
+
     dependencies = [
     ]
 
@@ -26,14 +15,6 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.SmallIntegerField(unique=True, serialize=False, primary_key=True)),
                 ('title', models.CharField(max_length=200)),
-                ('description', models.CharField(max_length=1000)),
-                ('units', models.SmallIntegerField()),
-                ('corequirements', models.CharField(max_length=200)),
-                ('prerequirements', models.CharField(max_length=200)),
-                ('fallsemester', models.BooleanField()),
-                ('springsemester', models.BooleanField()),
-                ('summersemester', models.BooleanField()),
-                ('availablenextsemester', models.BooleanField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
             ],
@@ -59,5 +40,4 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(to='course.Department'),
             preserve_default=True,
         ),
-        migrations.RunPython(load_fixture),
     ]
